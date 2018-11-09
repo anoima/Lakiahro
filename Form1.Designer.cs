1c591452c82e85e8ad754ecec1c30983dae06914
Bánka Milán <bankamilan22@gmail.com>
2018-11-09 15:39:33 +0100

    Created design
    sds

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+namespace lakiahrotest2
+{
+    partial class Helper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.asdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x14ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closebtn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tooltxt = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(702, 702);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(350, 755);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(100, 30);
+            this.startbtn.TabIndex = 1;
+            this.startbtn.Text = "New";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asdToolStripMenuItem,
+            this.x12ToolStripMenuItem,
+            this.x14ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // asdToolStripMenuItem
+            // 
+            this.asdToolStripMenuItem.Name = "asdToolStripMenuItem";
+            this.asdToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.asdToolStripMenuItem.Text = "10x10";
+            this.asdToolStripMenuItem.Click += new System.EventHandler(this.asdToolStripMenuItem_Click);
+            // 
+            // x12ToolStripMenuItem
+            // 
+            this.x12ToolStripMenuItem.Name = "x12ToolStripMenuItem";
+            this.x12ToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.x12ToolStripMenuItem.Text = "12x12";
+            this.x12ToolStripMenuItem.Click += new System.EventHandler(this.x12ToolStripMenuItem_Click);
+            // 
+            // x14ToolStripMenuItem
+            // 
+            this.x14ToolStripMenuItem.Name = "x14ToolStripMenuItem";
+            this.x14ToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.x14ToolStripMenuItem.Text = "14x14";
+            this.x14ToolStripMenuItem.Click += new System.EventHandler(this.x14ToolStripMenuItem_Click);
+            // 
+            // closebtn
+            // 
+            this.closebtn.Location = new System.Drawing.Point(672, 755);
+            this.closebtn.Name = "closebtn";
+            this.closebtn.Size = new System.Drawing.Size(100, 30);
+            this.closebtn.TabIndex = 3;
+            this.closebtn.Text = "Exit";
+            this.closebtn.UseVisualStyleBackColor = true;
+            this.closebtn.Click += new System.EventHandler(this.closebtn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(628, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // tooltxt
+            // 
+            this.tooltxt.AutoSize = true;
+            this.tooltxt.Location = new System.Drawing.Point(578, 9);
+            this.tooltxt.Name = "tooltxt";
+            this.tooltxt.Size = new System.Drawing.Size(34, 13);
+            this.tooltxt.TabIndex = 5;
+            this.tooltxt.Text = "Tool :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(734, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 21);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(10, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 702);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(40, 23);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(702, 25);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
+            // 
+            // Helper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 796);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tooltxt);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.closebtn);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox3);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Helper";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Helper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem asdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x12ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x14ToolStripMenuItem;
+        private System.Windows.Forms.Button closebtn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label tooltxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
